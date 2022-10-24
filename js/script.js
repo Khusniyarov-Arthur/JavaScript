1c57090f182d8df7397d025fbfa5ac608b772d86
khusniyarovaa <king_aptyp@mail.ru>
2022-10-24 21:20:36 +0300

    lesson-01

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let title = "lesson 01"
+let screens = "full screen"
+let screenPrice = "Over price"
+let rollback = "Не знаю что писать"
+let fullPrice = "free"
+let adaptive = "yes"
+
+alert(rollback);
+
+console.log(screens);
