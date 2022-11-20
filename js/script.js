375647ca3d6d2f8bb75925587c38919f872aaa92
khusniyarovaa <king_aptyp@mail.ru>
2022-11-20 16:08:01 +0300

    bot moved to a separate file

--- js/script.js
+++ js/script.js
@@ -113,39 +113,3 @@ console.log(getTitle());
 console.log(getRollbackMessage(fullPrice));
 console.log(servicePercentPrice2); 
  
-// Урок №6
-// Написать игровой бот.
-// "Загадывание случайного числа от 1 до 100"
-
-// проверку на число тут закоментировал т.к. она есть выше, 
-// но оставил тут для удобства проверки
-
-// let isNumber = function(num) {
-//   return !isNaN(parseFloat(num)) && isFinite(num)
-// }
-let hiddenNumber = 55;
-function game(hiddenNumber) {
-  function condition() {
-    answer = prompt("Угадай число от 1 до 100");
-    if (answer === null) {
-      alert("Игра окончена")
-    } else {
-      if (isNumber(answer)) {
-        if (hiddenNumber < answer) {
-          alert("Загаданное число меньше");
-          condition();
-        } else if (hiddenNumber > answer) {
-          alert("Загаданное число больше");
-          condition();
-        } else if (hiddenNumber = answer) {
-          alert("Поздравляю, Вы угадали!!!");
-        }
-      } else {
-        alert('Введите число'); 
-        condition();
-      }
-    }
-  }
-  condition();
-}
-game(hiddenNumber);
